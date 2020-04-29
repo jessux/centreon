290c200de6b2f8331919acbced1651a3d4348b25
Gabriel KAHLOUCHE <gabriel.kahlouche@gmail.com>
2020-04-29 14:45:58 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,14 @@
+import logging
+
+#Centreon URL
+URL="http://192.168.1.201"
+#Centreon restApi user/password
+USERNAME="REST"
+PASSWORD="restAPI"
+#Path to the file
+LOG_FILENAME="log"
+#console, file, both
+LOG_TYPE="console"
+LOG_LEVEL=logging.INFO
+#Centreon restAPI TOKEN
+TOKEN=""
